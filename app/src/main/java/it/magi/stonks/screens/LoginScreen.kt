f7bbb4c29596bc8428ecf5972904262202262ba1
Yari <92716846+Yari17@users.noreply.github.com>
2024-05-24 14:48:50 +0200

    BackgroundUpdated

--- app/src/main/java/it/magi/stonks/screens/LoginScreen.kt
+++ app/src/main/java/it/magi/stonks/screens/LoginScreen.kt
@@ -1,8 +1,10 @@
 package it.magi.stonks.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -24,6 +26,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -44,71 +48,75 @@ fun LoginScreen(navController: NavController) {
     var password by rememberSaveable {
         mutableStateOf("")
     }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = LoginBgColor)
-            .wrapContentHeight(Alignment.CenterVertically),
-    ) {
-        Text(
-            text = stringResource(id = R.string.app_name),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            textAlign = TextAlign.Center,
-            fontStyle = FontStyle.Italic,
-            fontFamily = titleFont(),
-            fontSize = fontSize,
-            color = Color.White
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.login_background_design),
+            contentDescription = "",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
         )
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
             modifier = Modifier
-                .background(color = LoginFormBgColor)
+                .fillMaxSize()
+                .wrapContentHeight(Alignment.CenterVertically),
         ) {
-
-            OutlinedTextField(
-                label = {
-                    Text(
-                        text = stringResource(id = R.string.login_email_label),
-                        color = Color.White
-                    )
-                },
-                value = email,
-                onValueChange = { email = it },
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedTextColor = Color.White,
-                    unfocusedTextColor = Color.White
-                )
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                textAlign = TextAlign.Center,
+                fontStyle = FontStyle.Italic,
+                fontFamily = titleFont(),
+                fontSize = fontSize,
+                color = Color.White
             )
-            OutlinedTextField(
-                label = {
-                    Text(
-                        text = stringResource(id = R.string.login_password_label),
-                        color = Color.White
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+
+                OutlinedTextField(
+                    label = {
+                        Text(
+                            text = stringResource(id = R.string.login_email_label),
+                            color = Color.White
+                        )
+                    },
+                    value = email,
+                    onValueChange = { email = it },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White
                     )
-                },
-                value = password,
-                onValueChange = { password = it },
-                colors = OutlinedTextFieldDefaults.colors(
-                    focusedTextColor = Color.White,
-                    unfocusedTextColor = Color.White
                 )
-            )
-            Button(onClick = {}) {
-                Text(text = stringResource(id = R.string.login_button_label))
+                OutlinedTextField(
+                    label = {
+                        Text(
+                            text = stringResource(id = R.string.login_password_label),
+                            color = Color.White
+                        )
+                    },
+                    value = password,
+                    onValueChange = { password = it },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedTextColor = Color.White,
+                        unfocusedTextColor = Color.White
+                    )
+                )
+                Button(onClick = {}) {
+                    Text(text = stringResource(id = R.string.login_button_label))
 
-            }
-            GoogleLogin()
+                }
+                GoogleLogin()
 
-            TextButton(onClick = { navController.navigate("signup") }) {
-                Text(text = stringResource(id = R.string.login_signup_label))
+                TextButton(onClick = { navController.navigate("signup") }) {
+                    Text(text = stringResource(id = R.string.login_signup_label))
 
+                }
             }
         }
     }
-
 }
 
 
