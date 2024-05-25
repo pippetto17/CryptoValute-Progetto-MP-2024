0f33d9fb9e568e1ddc895095cd49221706c60dea
pippetto17 <filippocarletti028@gmail.com>
2024-05-25 15:04:39 +0200

    Update MainActivity.kt

--- app/src/main/java/it/magi/stonks/activities/MainActivity.kt
+++ app/src/main/java/it/magi/stonks/activities/MainActivity.kt
@@ -3,9 +3,18 @@ package it.magi.stonks.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
 import it.magi.stonks.screens.LoginScreen
+import it.magi.stonks.screens.SignUpScreen
 import it.magi.stonks.ui.theme.StonksTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,29 +22,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
-
+        enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
+            LocalContext.current
             StonksTheme {
-                LoginScreen().Login(navController = navController)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding),
+                        startDestination = "login"
+                    ) {
+                        composable("login") {
+                            LoginScreen().Login(navController = navController)
+                        }
+                        composable("signup") {
+                            SignUpScreen(navController)
+                        }
+                    }
+                }
             }
         }
     }
 }
-
-/*@Composable
-fun SplashPage() {
-    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.animation))
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(LoginBgColor)
-    ) {
-        LottieAnimation(
-            composition = composition,
-
-            )
-    }
-
-}*/
