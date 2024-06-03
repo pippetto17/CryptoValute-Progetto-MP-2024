83b6a122aadeafadc4a456b1aae77185e6e56ac0
pippetto17 <filippocarletti028@gmail.com>
2024-06-04 00:31:49 +0200

    fix searchscreen

---
+++ app/src/main/java/it/magi/stonks/screens/SearchScreen.kt
@@ -0,0 +1,21 @@
+package it.magi.stonks.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun SearchScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Search Screen")
+    }
+}
